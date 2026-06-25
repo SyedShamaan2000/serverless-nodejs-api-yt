7de8adc13acfd97897c28c3e2b5bbfd6c8a064c5
Syed <syedshamaan14@gmail.com>
2026-06-25 21:26:27 +0530

    Add DATABASE_URL environment variable and update .gitignore

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from root!",
+    databaseUrl: process.env.DATABASE_URL || "No database URL set",
   });
 });
 
